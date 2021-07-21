08b4884ab00e181f00725844e32d76e5ccff18f3
tushar <tushar@oc8182202687.ibm.com>
2021-07-21 17:37:14 +0530

    changes with gson

--- CSVtoJSON/src/createJSON.java
+++ CSVtoJSON/src/createJSON.java
@@ -7,8 +7,10 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 
 
 public class createJSON {  
@@ -19,64 +21,60 @@ public class createJSON {
     	String country="";
     	String population="";
     	String line="";
-    	 
+    	String gdp="";
+    	Gson gson = new GsonBuilder().setPrettyPrinting().create();
     	String fileName = "/home/tushar/work/csvtojson/country.csv";
     	String jsonfile="/home/tushar/work/csvtojson/final.json";
-    	 Scanner scanner = new Scanner(new File(fileName));
-    	 File jfile=new File(jsonfile);  
-    	jfile.delete();
-    	 jfile.createNewFile();  
+    	  Scanner scanner = new Scanner(new File(fileName));
+    	    File jfile=new File(jsonfile);
+    	    jfile.delete();
+    	    jfile.createNewFile();
 
-         FileWriter fileWriter = new FileWriter(jfile,true); 
-         fileWriter.write("{ \"countries\":[");
-         //Set the delimiter used in file
-        while( scanner.hasNextLine())
-        {
-        line=scanner.nextLine();
-         Scanner linesc=new Scanner(line);
-         linesc.useDelimiter(",");
-          
-         //Get all tokens and store them in some data structure
-         //I am just printing them
-         while (linesc.hasNext()) 
-         {
-        	 country=linesc.next();
-        	 population=linesc.next();
-        	
-             System.out.println("Country | Population = "+country+"|"+population);
-            country=country.replaceAll("\\n", "");
-            population=population.replaceAll("\\n", "");
-            //System.out.println("country :"+country);
-            //System.out.println("population: "+population); 
-            JSONObject countryObj = new JSONObject();  
-             countryObj.put("Name", country);  
-             countryObj.put("Population", population);  
-             JSONArray listOfStates = new JSONArray();  
-             listOfStates.add("Madhya Pradesh");  
-             listOfStates.add("Maharastra");  
-             listOfStates.add("Rajasthan");  
+    	    FileWriter fileWriter = new FileWriter(jfile,true);
+    	    fileWriter.write("{ \"countries\":[");
+    	    //Set the delimiter used in file
+    	    while( scanner.hasNextLine())
+    	    {
+    	        line=scanner.nextLine();
+    	        Scanner linesc=new Scanner(line);
+    	        linesc.useDelimiter(",");
 
-             countryObj.put("States", listOfStates);  
-            // System.out.println("json obj"+countryObj);
-             
-             
-            // System.out.println("Writing JSON object to file");  
-             //System.out.println("-----------------------");  
-             //System.out.print(countryObj);  
-            
-             fileWriter.write(countryObj.toJSONString());  
-            if(scanner.hasNext()) {
-            fileWriter.write(",");
-            }
-         }
-        }
-        
-         fileWriter.write("]}");
-         fileWriter.close();
-         //Do not forget to close the scanner 
-        
-         scanner.close();  
+    	       while (linesc.hasNext())
+    	        {
+    	            country=linesc.next();
+    	            population=linesc.next();
+    	            gdp = linesc.next();
 
-    }  
-} 
+    	            System.out.println("Country | Population | GDP = "+country+"|"+population+"|"+gdp);
+    	            country=country.replaceAll("\\n", "");
+    	            population=population.replaceAll("\\n", "");
+    	            gdp=gdp.replaceAll("\\n", "");
 
+    	            JsonObject countryObj = new JsonObject();
+    	            countryObj.addProperty("Country", country);
+    	            
+    	            countryObj.addProperty("Population", population);
+    	            countryObj.addProperty("GDP", gdp);
+
+    	            final JsonArray countryObjs = new JsonArray();
+    	            countryObjs.add(countryObj);
+
+    	            final String jsonline=gson.toJson(countryObjs);
+    	           // System.out.println("Print ready:" +countryObj.toJSONString());
+    	            System.out.println("Final print : "+jsonline);
+    	            fileWriter.write(jsonline);
+
+    	            if(scanner.hasNext()) {
+    	                fileWriter.write(",");
+    	            }
+    	        }
+    	    }
+
+    	    fileWriter.write("]}");
+    	    fileWriter.close();
+    	    //Do not forget to close the scanner
+
+    	    scanner.close();
+
+    	}
+    }
